caaf8ef410ca6102eb66e72d8fd982c18febbddd
Selen <selenyanar@gmail.com>
2021-04-05 19:18:42 +0300

    Add NavigationView to LaunchView

--- Anbulance/Anbulance/ViewController/LaunchView.swift
+++ Anbulance/Anbulance/ViewController/LaunchView.swift
@@ -10,41 +10,55 @@ import SwiftUI
 struct LaunchView: View {
     
     var body: some View {
-        VStack {
-            Image("AnbulanceLogo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.horizontal, 50.0)
-                .edgesIgnoringSafeArea(.all)
-            Text("Anbulance")
-                .fontWeight(.heavy)
-                .foregroundColor(Color("AnbulanceBlue"))
-                .font(.system(size: 40))
-                .padding(.bottom, 100.0)
-            Button(
-                action: {
-                    print("Giriş yap ekranına git")},
-                label: {
-                        RoundedRectangle(cornerRadius: 5)
-                        .frame(width: 250, height: 50, alignment: .center)
-                        .foregroundColor(.gray)
-                        .padding()
+        NavigationView {
+            VStack {
+                Image("AnbulanceLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 50.0)
+                    .edgesIgnoringSafeArea(.all)
+                Text("Anbulance")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color("AnbulanceBlue"))
+                    .font(.system(size: 40))
+                    .padding(.bottom, 100.0)
+                NavigationLink(
+                    destination: LoginView(),
+                    label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: 250, height: 50, alignment: .center)
+                                .foregroundColor(.gray)
+                                .padding()
+                            Text("Giriş yap")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                        }
                     })
-            Button(
-                action: {
-                    print("Kaydol ekranına git")},
-                label: {
-                    RoundedRectangle(cornerRadius: 5)
-                        .frame(width: 250, height: 50, alignment: .center)
-                        .foregroundColor(Color("AnbulanceBlue"))
+                NavigationLink(
+                    destination: SignUpView(),
+                    label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: 250, height: 50, alignment: .center)
+                                .foregroundColor(Color("AnbulanceBlue"))
+                            Text("Kaydol")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                        }
                     })
-                
-        }
+            }
+            
+        }.offset(y: -40)
     }
 }
+
 
 struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
         LaunchView()
+            .preferredColorScheme(.light)
     }
 }
