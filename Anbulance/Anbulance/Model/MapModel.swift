284b756257e523cdaa7cc8b6226759408bac370e
Selen <selenyanar@gmail.com>
2021-04-17 12:37:52 +0300

    Add custom AnnotationView image.

--- Anbulance/Anbulance/Model/MapModel.swift
+++ Anbulance/Anbulance/Model/MapModel.swift
@@ -5,25 +5,21 @@
 //  Created by Selen Yanar on 9.04.2021.
 //
 
-import Foundation
 import UIKit
 import SwiftUI
 import MapKit
 
 
 struct MapModel: UIViewRepresentable {
-
+    
+    let view = UIView()
+    let map = MKMapView()
+    
+    let yedikuleShelter = ShelterAnnotation(title: "Yedikule Hayvan Barınağı", coordinate: CLLocationCoordinate2D(latitude: 40.9907822322955, longitude: 28.920995484659933))
     
     typealias UIViewType = UIView
     
     func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        let coordinate = CLLocationCoordinate2D(latitude: 41.01224, longitude: 28.976018)
-        let map = MKMapView()
-        map.setRegion(MKCoordinateRegion(center: coordinate,
-                                         span: MKCoordinateSpan(latitudeDelta: 0.7,
-                                                                longitudeDelta: 0.7)),
-                      animated: true)
         
         view.addSubview(map)
         
@@ -37,16 +33,65 @@ struct MapModel: UIViewRepresentable {
             
         ])
         
-        let pin = MKPointAnnotation()
-        pin.coordinate = CLLocationCoordinate2D(latitude: 40.9907822322955, longitude: 28.920995484659933)
-        pin.title = "Yedikule Hayvan Barınağı"
-        map.addAnnotation(pin)
+        //   let pin = MKPointAnnotation()
+        //   pin.coordinate = CLLocationCoordinate2D(latitude: 40.9907822322955, longitude: 28.920995484659933)
+        //   pin.title = "Yedikule Hayvan Barınağı"
+        //    map.addAnnotation(pin)
+        
         
         return view
     }
     
+    
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+        var mapModelController: MapModel
+        
+        init(_ control: MapModel) {
+            self.mapModelController = control
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor
+                        annotation: MKAnnotation) -> MKAnnotationView?{
+            //Custom View for Annotation
+            let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customView")
+            annotationView.canShowCallout = true
+            //Your custom image icon
+            annotationView.image = #imageLiteral(resourceName: "AnbulancePin")
+            annotationView.frame.size = CGSize(width: 50, height: 50)
+            return annotationView
+        }
+    }
+    
+    class ShelterAnnotation: NSObject, MKAnnotation {
+        let title: String?
+        let coordinate: CLLocationCoordinate2D
+        init(title: String?,
+             coordinate: CLLocationCoordinate2D) {
+            self.title = title
+            self.coordinate = coordinate
+        }
+    }
+    
+    
+    
+    
     func updateUIView(_ uiView: UIView, context: Context) {
-        // do nothing
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 41.01224, longitude: 28.976018)
+        let span = MKCoordinateSpan(latitudeDelta: 0.7,
+                                    longitudeDelta: 0.7)
+        map.setRegion(MKCoordinateRegion(center: coordinate,
+                                         span: span),
+                      animated: true)
+        
+        map.delegate = context.coordinator
+        map.addAnnotation(yedikuleShelter)
+        
+        
+    }
+    
+    func makeCoordinator() -> MapViewCoordinator{
+        MapViewCoordinator(self)
     }
     
 }
