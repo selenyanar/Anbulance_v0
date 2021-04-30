a5dbf0eedf0beef57f974962bb84bdb448a2c90d
Selen <selenyanar@gmail.com>
2021-04-30 13:52:12 +0300

    Sign-in & Register users w/ Firebase.
    - Add ErrorView
    - Show password mismatch error
    - Show Firebase local errors

--- Anbulance/Anbulance/ViewController/LoginView.swift
+++ Anbulance/Anbulance/ViewController/LoginView.swift
@@ -6,78 +6,139 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct LoginView: View {
     
     @State var email = ""
     @State var parola = ""
     @State var visible = false
+    @State var alert = false
+    @State var error = ""
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
     
     var body: some View {
         
-        VStack {
-            Image("AnbulanceLogo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(80.0)
-                .edgesIgnoringSafeArea(.all)
-            HStack {
-                Image(systemName: "envelope")
-                    .foregroundColor(.gray)
-                TextField("Email", text: self.$email)
-                    .frame(width: 300, height: 50, alignment: .center)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .cornerRadius(8)
-            }
-            HStack {
+        ZStack {
+            
+            ZStack {
                 
-                Button(action: {
-                    self.visible.toggle()
+                
+                VStack {
                     
-                }) {
-                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                        .foregroundColor(Color("AnbulanceBlue"))
+                    if self.status {
+                        
+                        MapView()
+                        
+                    } else {
+                        
+                        VStack {
+                            Image("AnbulanceLogo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(80.0)
+                                .edgesIgnoringSafeArea(.all)
+                            HStack {
+                                Image(systemName: "envelope")
+                                    .foregroundColor(.gray)
+                                TextField("Email", text: self.$email)
+                                    .frame(width: 300, height: 50, alignment: .center)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .cornerRadius(8)
+                            }
+                            HStack {
+                                
+                                Button(action: {
+                                    self.visible.toggle()
+                                    
+                                }) {
+                                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                        .foregroundColor(Color("AnbulanceBlue"))
+                                }
+                                
+                                if self.visible {
+                                    TextField("Parola", text: self.$parola)
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                        .frame(width: 300, height: 50, alignment: .center)
+                                        .cornerRadius(8)
+                                    
+                                } else {
+                                    SecureField("Parola", text: self.$parola)
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                        .frame(width: 300, height: 50, alignment: .center)
+                                        .cornerRadius(8)
+                                    
+                                }
+                                
+                            }
+                            
+                            HStack {
+                                Button(action: {
+                                    //Forgot password action
+                                }) {
+                                    
+                                    Text("Parolamı unuttum")
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color("AnbulanceBlue"))
+                                        .padding(.leading, 180.0)
+                                    
+                                }
+                            }
+                            
+                            Button(
+                                action: {
+                                    self.verify()},
+                                label: {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .frame(width: 300, height: 50, alignment: .center)
+                                        .foregroundColor(Color("AnbulanceBlue"))
+                                        .cornerRadius(8)
+                                        .padding(.top, 100.0)
+                                })
+                        }
+                    }
                 }
-                
-                if self.visible {
-                    TextField("Parola", text: self.$parola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
-                    
-                } else {
-                    SecureField("Parola", text: self.$parola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
+                .onAppear {
+                    NotificationCenter.default.addObserver(forName: NSNotification.Name("status"), object: nil, queue: .main) { (_) in
+                        
+                        self.status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                        
+                    }
                     
                 }
-                
             }
             
-            HStack {
-                Button(action: {
-                    //Forgot password action
-                }) {
-                 
-                   Text("Parolamı unuttum")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("AnbulanceBlue"))
-                    .padding(.leading, 180.0)
+            if self.alert {
+                
+                ErrorView(alert: self.$alert, error: self.$error)
+                
+            }
+        }
+    }
+    
+    func verify() {
+        
+        if self.email != "" && self.parola != "" {
+            
+            Auth.auth().signIn(withEmail: self.email, password: self.parola) { res, err in
+                
+                if err != nil{
                     
+                    self.error = err!.localizedDescription
+                    self.alert.toggle()
+                    return
                 }
+                
+                print("Başarılı")
+                UserDefaults.standard.set(true, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
             }
             
-            Button(
-                action: {
-                    print("Giriş yap")},
-                label: {
-                    RoundedRectangle(cornerRadius: 5)
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .foregroundColor(Color("AnbulanceBlue"))
-                        .cornerRadius(8)
-                        .padding(.top, 100.0)
-                })
+            
+        } else {
+            self.error = "Lütfen bütün alanları doldurun"
+            self.alert.toggle()
+            
         }
     }
 }
@@ -87,3 +148,56 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
+
+
+struct ErrorView: View {
+    
+    @State var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error: String
+    
+    var body: some View {
+        
+        ZStack {
+            
+            VStack {
+                
+                HStack {
+                    Text("Hata")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 25)
+                
+                Text(self.error)
+                    .foregroundColor(self.color)
+                    .padding(.top)
+                    .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.alert.toggle()
+                    
+                }) {
+                    
+                    Text("İptal")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width-120)
+                }
+                .background(Color("AnbulanceBlue"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+            }
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width-70)
+            .background(Color.white)
+            .cornerRadius(15)
+            .border(Color("AnbulanceBlue"))
+        }
+    }
+}
+
--- Anbulance/Anbulance/ViewController/SignUpView.swift
+++ Anbulance/Anbulance/ViewController/SignUpView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct SignUpView: View {
     
@@ -16,103 +17,157 @@ struct SignUpView: View {
     @State var reparola = ""
     @State var visible = false
     @State var revisible = false
+    @State var alert = false
+    @State var error = ""
     
     var body: some View {
-        VStack {
-            Image("AnbulanceLogo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding([.top, .leading, .trailing], 60.0)
-                .edgesIgnoringSafeArea(.all)
-            HStack {
-                Image(systemName: "person")
-                    .foregroundColor(.gray)
-                TextField("Ad", text: $ad)
-                    .frame(width: 300, height: 50, alignment: .center)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .cornerRadius(8)
-            }.padding(.top, 10.0)
-            HStack {
-                Image(systemName: "person")
-                    .foregroundColor(.gray)
-                TextField("Soyad", text: $soyad)
-                    .frame(width: 300, height: 50, alignment: .center)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .cornerRadius(8)
-            }
-            HStack {
-                Image(systemName: "envelope")
-                    .foregroundColor(.gray)
-                TextField("Email", text: $email)
-                    .frame(width: 300, height: 50, alignment: .center)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .cornerRadius(8)
-            }
-            HStack {
-                Button(action: {
-                    self.visible.toggle()
-                    
-                }) {
-                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                        .foregroundColor(Color("AnbulanceBlue"))
-                }
-                
-                if self.visible {
-                    TextField("Parola oluştur", text: self.$parola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
-                    
-                } else {
-                    SecureField("Parola oluştur", text: self.$parola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
-                    
-                }
-                
-            }
-            HStack {
-                Button(action: {
-                    self.revisible.toggle()
-                    
-                }) {
-                    Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
-                        .foregroundColor(Color("AnbulanceBlue"))
-                }
-                
-                if self.revisible {
-                    TextField("Parolayı yeniden gir", text: self.$reparola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
-                    
-                } else {
-                    SecureField("Parolayı yeniden gir", text: self.$reparola)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 300, height: 50, alignment: .center)
-                        .cornerRadius(8)
-                    
-                }
-            }.padding(.bottom, 150.0)
+        
+        ZStack {
             
-            Button(action: {
-                //REGISTER FIREBASE
-            }) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 5)
+            VStack {
+                Image("AnbulanceLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.top, .leading, .trailing], 60.0)
+                    .edgesIgnoringSafeArea(.all)
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.gray)
+                    TextField("Ad", text: $ad)
                         .frame(width: 300, height: 50, alignment: .center)
-                        .foregroundColor(Color("AnbulanceBlue"))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .cornerRadius(8)
-                    Text("Kaydol")
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .font(.system(size: 18))
-                }.padding(.bottom, 20)
+                }.padding(.top, 10.0)
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.gray)
+                    TextField("Soyad", text: $soyad)
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .cornerRadius(8)
+                }
+                HStack {
+                    Image(systemName: "envelope")
+                        .foregroundColor(.gray)
+                    TextField("Email", text: $email)
+                        .frame(width: 300, height: 50, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .cornerRadius(8)
+                }
+                HStack {
+                    Button(action: {
+                        self.visible.toggle()
+                        
+                    }) {
+                        Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(Color("AnbulanceBlue"))
+                    }
+                    
+                    if self.visible {
+                        TextField("Parola oluştur", text: self.$parola)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .cornerRadius(8)
+                        
+                    } else {
+                        SecureField("Parola oluştur", text: self.$parola)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .cornerRadius(8)
+                        
+                    }
+                    
+                }
+                HStack {
+                    Button(action: {
+                        self.revisible.toggle()
+                        
+                    }) {
+                        Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(Color("AnbulanceBlue"))
+                    }
+                    
+                    if self.revisible {
+                        TextField("Parolayı yeniden gir", text: self.$reparola)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .cornerRadius(8)
+                        
+                    } else {
+                        SecureField("Parolayı yeniden gir", text: self.$reparola)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .cornerRadius(8)
+                        
+                    }
+                }.padding(.bottom, 130.0)
+                
+                Button(action: {
+                    //REGISTER FIREBASE
+                    
+                    self.register()
+                    
+                }) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .foregroundColor(Color("AnbulanceBlue"))
+                            .cornerRadius(8)
+                        Text("Kaydol")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .font(.system(size: 18))
+                    }.padding(.bottom, 20)
+                }
+                
             }
+            
+            if self.alert{
+                
+                ErrorView(alert: self.$alert, error: self.$error)
+                
+            }
+        }
+    }
+    
+    func register() {
+        
+        if self.email != "" {
+            
+            if self.parola == self.reparola {
+                
+                Auth.auth().createUser(withEmail: self.email, password: self.parola) { (ress, err) in
+                    
+                    if err != nil {
+                        
+                        self.error = err!.localizedDescription
+                        self.alert.toggle()
+                        return
+                        
+                    }
+                    
+                    print("success")
+                    
+                    UserDefaults.standard.set(true, forKey: "status")
+                    NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                    
+                }
+                
+                
+            } else {
+                
+                self.error = "Parolalar eşleşmiyor"
+                self.alert.toggle()
+                
+            }
+        } else {
+            
+            self.error = "Lütfen bütün alanları doldurun"
+            self.alert.toggle()
             
         }
     }
+    
 }
 
 struct SignUpView_Previews: PreviewProvider {
